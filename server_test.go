ae7f35b31663455b5b7d1bddc3ae196f09af24f3
Ehsan Mohammadi <ehsanx64@gmail.com>
2023-12-14 00:41:47 +0330

    test: add test for user save endpoint

--- server_test.go
+++ server_test.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"testing"
 	"time"
 )
@@ -19,16 +21,29 @@ type ReqResult struct {
 	Response *http.Response
 }
 
-func httpGet(url string) ReqResult {
+type UrlTest struct {
+	Url    string
+	Test   string
+	Method string
+	Data   []byte
+}
+
+func httpCall(u UrlTest) ReqResult {
 	verbose := false
+	data := bytes.NewBuffer([]byte(u.Data))
 	out := ReqResult{}
 	client := http.Client{
 		Timeout: 30 * time.Second,
 	}
 
-	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, url, nil)
+	req, err := http.NewRequestWithContext(context.Background(), u.Method, u.Url, data)
 	if err != nil {
 		panic(err)
+	}
+
+	if string(u.Data) != "" {
+		// Following must be added in order for urlencoded post request body to be submitted correctly
+		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	}
 
 	res, err := client.Do(req)
@@ -49,34 +64,71 @@ func httpGet(url string) ReqResult {
 		}
 
 		fmt.Printf("res: %v\n", string(body))
+		fmt.Printf("req: %v\n", req)
 	}
 
 	return out
 }
 
-var urls = make(map[string]map[string]string)
+var urls = make(map[string]UrlTest)
 
-func addUrl(u string, t string) {
-	urls[u] = map[string]string{
-		"url":  defaultUrl + u,
-		"test": t,
+func addUrl(url string, u UrlTest) {
+	method := http.MethodGet
+
+	if u.Method != "" {
+		method = u.Method
+	}
+
+	urls[url] = UrlTest{
+		Url:    defaultUrl + url,
+		Test:   u.Test,
+		Method: method,
+		Data:   u.Data,
 	}
 }
 
 func Test_main(t *testing.T) {
-	addUrl("/", "Hello!!!\n")
-	addUrl("/welcome", "Welcome User!!!\n")
-	addUrl("/welcome/em", "Welcome EM!!!\n")
-	addUrl("/welcome/adam", "Welcome Adam!!!\n")
-	addUrl("/users/show?team=alpha&member=subzero", fmt.Sprintf("team: %s, member: %s\n", "alpha", "subzero"))
-	addUrl("/users/show?team=alpha&member=", fmt.Sprintf("team: %s, member: %s\n", "alpha", ""))
+	addUrl("/", UrlTest{
+		Test: "Hello!!!\n",
+	})
+
+	addUrl("/welcome", UrlTest{
+		Test: "Welcome User!!!\n",
+	})
+
+	addUrl("/welcome/em", UrlTest{
+		Test: "Welcome EM!!!\n",
+	})
+
+	addUrl("/welcome/adam", UrlTest{
+		Test: "Welcome Adam!!!\n",
+	})
+
+	addUrl("/users/show?team=alpha&member=subzero", UrlTest{
+		Test: fmt.Sprintf("team: %s, member: %s\n", "alpha", "subzero"),
+	})
+
+	addUrl("/users/show?team=alpha&member=", UrlTest{
+		Test: fmt.Sprintf("team: %s, member: %s\n", "alpha", ""),
+	})
+
+	// Generate request body for /users/save endpoint
+	saveData := url.Values{}
+	saveData.Set("name", "Adam Smith")
+	saveData.Set("email", "adam@earth.org")
+
+	addUrl("/users/save", UrlTest{
+		Test:   fmt.Sprintf("name: %s, email: %s\n", "Adam Smith", "adam@earth.org"),
+		Method: http.MethodPost,
+		Data:   []byte(saveData.Encode()),
+	})
 
 	for k, v := range urls {
 		fmt.Println("<=", k)
-		req := httpGet(v["url"])
+		req := httpCall(v)
 		fmt.Println("=>", req.Body)
-		if req.Body != v["test"] {
-			t.Error("!!! "+v["test"]+" <=> ", req.Body)
+		if req.Body != v.Test {
+			t.Error("!!! "+v.Test+" <=> ", req.Body)
 		}
 	}
 }
